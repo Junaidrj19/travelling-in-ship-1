202d73043b7aadbe3a12b65b4c5c08a5530d00da
Junaidrj19 <89200291+Junaidrj19@users.noreply.github.com>
2021-08-31 22:22:11 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+
+function preload(){
+
+}
+
+function setup(){
+  createCanvas(400,400);
+var ship = createSprite(400,400,20,20)
+ship.addAnimation()
+}
+
+function draw() {
+  background("blue");
+ 
+}
